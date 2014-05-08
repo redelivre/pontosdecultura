4b3e648a78d9dbb5dd62eb80c07b270d8b36453c
Eduardo Zulian <eduardozulian@gmail.com>
2014-05-08 16:26:26 -0300

    Change structure

--- home.php
+++ home.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 	<div class="container cf">
-		<section class="row search-form">
+		<section class="search-form">
 			<?php get_search_form(); ?>
 		</section>
 
-		<section class="row search-result">
-			<div class="span map-col">
+		<section class="search-result">
+			<div class="map-col">
 				<?php mapasdevista_view_results(); ?>
 			</div>
-			<div class="span map-col">
+			<div class="map-col">
 				<div class="map clear"><?php Pontosdecultura::the_map(); ?></div>
 			</div>
 		</section>
 
-		<section class="row search-estado">
+		<section class="search-estado clearfix">
 			<?php include 'map.php'; ?>
 		</section>
 
-		<section class="row search-tags">
+		<section class="search-tags">
 		</section>
 		
-		<section class="row search-avancado">
+		<section class="search-avancado">
 		</section>
 	</div><!-- /container -->
 <?php get_footer(); ?>
