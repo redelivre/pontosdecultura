df2825c9ec3f0097cfd257212dd0e0cc48fee286
Jacson Passold <jacson@ethymos.com.br>
2014-05-09 16:47:37 -0300

    add result template for home

---
+++ mapasdevista-loop-filter.php
@@ -0,0 +1,24 @@
+<?php $posts = mapasdevista_get_posts(1, $mapinfo); ?>
+<div id="results" class="clearfix">
+    <h3><?php _e('Results', 'mapasdevista'); ?> [<span id="filter_total"><?php echo $posts->post_count; //->found_posts;    ?></span>]</h3>
+    <div class="clear"></div>
+    <?php while($posts->have_posts()): $posts->the_post(); ?>
+        <div id="result_<?php the_ID(); ?>" class="result clearfix">
+            <div class="pin"><?php the_pin(); ?></div>
+            <div class="content">
+                <!-- <p class="metadata date bottom"><?php the_time(get_option('date_format')); ?></p> -->
+                <!-- the permalink to the post must have the js-link-to-post class. With this, mapasdevista will open the post over the map. 
+                It also have to have an id attribute with the ID of th target post. the id can be anything as long as the post ID is the only numeric part of it. -->
+                <h1 class="bottom"><a class="js-link-to-bubble" id="post-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <?php $mapTheme = get_option('mapasdevista_theme_options', array());
+			    if(!array_key_exists('show_authors', $mapTheme) || $mapTheme['show_authors'] == 'Y')
+			    { ?>
+	                <p class="metadata author"><?php _e('Published by', 'mapasdevista'); ?>
+	                    <a class="js-filter-by-author-link" href="<?php echo get_author_posts_url(get_the_ID()); ?>" id="author-link-<?php the_author_ID(); ?>" title="<?php esc_attr(the_author()); ?>"><?php the_author(); ?></a>
+	                </p>
+	               <?php 
+	            } ?>
+            </div>
+        </div>
+    <?php endwhile; ?>
+</div>
