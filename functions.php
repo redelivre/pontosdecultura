d534586d183301f2abd05cff4bd1faa98bcc79a5
Eduardo Zulian <eduardozulian@gmail.com>
2014-05-13 13:51:53 -0300

    Update site structure
    Start adding section colors too

--- functions.php
+++ functions.php
@@ -13,6 +13,54 @@ if ( ! isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
 }
 
+if ( ! function_exists( 'pontosdecultura_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function pontosdecultura_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on _s, use a find and replace
+	 * to change '_s' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'pontosdecultura', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	//add_theme_support( 'post-thumbnails' );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => __( 'Secondary Menu', 'pontosdecultura' ),
+	) );
+
+	// Enable support for Post Formats.
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+
+	// Enable support for HTML5 markup.
+	add_theme_support( 'html5', array(
+		'comment-list',
+		'search-form',
+		'comment-form',
+		'gallery',
+		'caption',
+	) );
+}
+endif; // pontosdecultura_setup
+add_action( 'after_setup_theme', 'pontosdecultura_setup' );
+
 /**
  * Register widget area.
  *
--- home.php
+++ home.php
@@ -1,25 +1,49 @@
 <?php get_header(); ?>
-	<section class="search-form search-area clearfix">
-		<?php get_search_form(); ?>
+	<section class="search-intro container search-area clearfix">
+		<div class="container">
+			<p class="search-text">
+				O Mapa Cultura Viva é Livre e aberto, fruto do acúmulo de dezenas de experiências da Rede de Pontos de
+				Cultura com o objetivo de dar visibilidade e articular iniciativas e ações do programa Cultura Viva.
+				 Existem várias formas de utilizar a plataforma, sendo possível fazer buscas de forma aberta ou avançada, 
+				 através do mapa ou pelos termos mais procurados. Participe interagindo com os pontos, por consultas 
+				 públicas ou atualizando informações. 
+			</p>
+			<?php get_search_form(); ?>
+		</div><!-- .container -->
 	</section>
 
 	<section class="search-result search-area clearfix">
-		<div class="search-result-list">
-			<?php mapasdevista_view_results(); ?>
-		</div>
-		<div class="search-result-map">
-			<div class="map clear"><?php Pontosdecultura::the_map(); ?></div>
-		</div>
+		<div class="container">
+			<div class="search-result-list">
+				<?php //mapasdevista_view_results(); ?>
+			</div>
+			<div class="search-result-map">
+				<div class="map clear"><?php //Pontosdecultura::the_map(); ?></div>
+			</div>
+		</div><!-- .container -->
 	</section>
 
 	<section class="search-estado search-area clearfix">
-		<?php include 'map.php'; ?>
+		<div class="container">
+			<?php include 'map.php'; ?>
+		</div><!-- .container -->
+	</section>
+
+	<section class="search-advanced search-area clearfix">
+		<div class="container">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div><!-- .container -->
 	</section>
 
 	<section class="search-tags search-area clearfix">
-		<?php wp_tag_cloud(); ?>
+		<div class="container">
+			<?php wp_tag_cloud(); ?>
+		</div><!-- .container -->
 	</section>
 	
-	<section class="search-avancado search-area clearfix">
-	</section>
 <?php get_footer(); ?>
