c9838599ad51281e3b242897f2bd12c86ff3fd9d
Jacson Passold <jacson@ethymos.com.br>
2014-05-21 14:57:50 -0300

    .filter-panel-tooglebox--toggled when toggled

--- js/map-page-scripts.js
+++ js/map-page-scripts.js
@@ -2,5 +2,6 @@ jQuery(document).ready(function()
 {
 	jQuery(".filter-panel-tooglebox-meta-head").click(function () {
 		jQuery(this).parent('div').children('.filter-panel-tooglebox-list').toggle(400);
+		jQuery(this).parent('div').toggleClass("filter-panel-tooglebox--toggled");
 	});
 });
