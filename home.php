96c0d7c23f789c4c5126f22dabc9c5f0f75ddd5e
Paulo Santana <phls00@gmail.com>
2014-08-19 15:44:25 -0300

    Fix layout

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+<?php get_header();  ?>
 	<div id="post_overlay">
 	        <a id="pontos_close_post_overlay" title="Fechar"><?php mapasdevista_image("close.png", array("alt" => "Fechar")); ?></a>
 	        <div id="post_overlay_content" class="post_overlay_content" >
