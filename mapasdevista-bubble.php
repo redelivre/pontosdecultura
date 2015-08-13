1a209845ffacba9ead75395afa59ee2391e93cbf
Jacson Passold <jacson@ethymos.com.br>
2015-08-13 15:50:58 -0300

    balloon

--- mapasdevista-bubble.php
+++ mapasdevista-bubble.php
@@ -1,5 +1,4 @@
-<div class="balloon-taxs" >
-	<?php
+<?php
 	$post_ID = get_the_ID();
 	
 	$mapinfo = get_option('mapasdevista', true);
@@ -15,7 +14,7 @@
 	$wpdb->posts.ID = $post_ID
 	AND $wpdb->posts.post_type in (".$pt.")
 	AND $wpdb->postmeta.meta_key = '_mpv_inmap'
-	AND $wpdb->term_taxonomy.taxonomy NOT IN ( 'territorio', 'tipo', 'publicoalvo', 'tematico', 'identitario' )
+	AND $wpdb->term_taxonomy.taxonomy = 'cenico-performativa'
 	GROUP BY $wpdb->terms.term_id
 	";
 	
@@ -40,7 +39,13 @@
 	}
 	
 	?>
-	<span class="balloon-taxs-names"><strong>Áreas culturais: </strong><?php echo $terms_str;//implode(',', $terms); ?></span>
+
+<div class="balloon-email" >
+	<span class="balloon-taxs-names"><strong>E-mail: </strong><?php echo get_post_meta($post_ID, 'pratica-email', true); ?></span>
+</div>
+<div class="balloon-taxs" >
+	
+	<span class="balloon-taxs-names"><strong>Área(s) da Pesquisa Cênico-Performativa(s): </strong><?php echo $terms_str;//implode(',', $terms); ?></span>
 
 </div>
 
@@ -50,7 +55,7 @@
     $balloon_excerpt = get_the_excerpt();
 
     if ( ! empty( $balloon_excerpt ) ) {
-    	echo '<strong>Objetivos: </strong>' . $balloon_excerpt;
+    	echo '<strong>Descrição da Pesquisa do Núcleo: </strong>' . $balloon_excerpt;
     }
     ?>
     
