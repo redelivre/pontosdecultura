a2136dad0baff99312c57b99efa8f60408372fe9
Flávio Zavan <flaviozavan@gmail.com>
2015-12-07 23:50:14 -0200

    Fix references to Praticas

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
 					em Artes Cênicas e Performativas no Brasil. <a href="%2$s">Cadastre sua pesquisa</a>, navegue pelo <a href="%3$s">mapa e pelos filtros</a>,
 					faça busca por palavras-chave. São diversas pesquisas e núcleos de trabalho perene em todo o Brasil.',
 					$home_url . 'o-projeto',
-					Praticas::get_new_url(),
+					EmRede::get_new_url(),
 					$home_url . 'mapa'
 				);
 				?>
@@ -260,7 +260,7 @@
 			</div>
 			<div class="search-not-here">
 				<img class="icon-capa" src="<?php echo get_template_directory_uri().'/images/icon.png'; ?>"/>
-				<?php printf( __( 'Não encontrou o procurava? Participe <a href="%s">cadastre sua pesquisa</a> ou <a href="%s">entre em contato</a> conosco!', 'pontosdecultura' ),  Praticas::get_new_url(), get_bloginfo( 'url' ) . '/contato' ); ?>
+				<?php printf( __( 'Não encontrou o procurava? Participe <a href="%s">cadastre sua pesquisa</a> ou <a href="%s">entre em contato</a> conosco!', 'pontosdecultura' ),  EmRede::get_new_url(), get_bloginfo( 'url' ) . '/contato' ); ?>
 			</div>
 		</div>
 	</section>
