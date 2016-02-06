5d7f631d878c33063216c4db754256fdbd96578e
Flávio Zavan <flaviozavan@gmail.com>
2016-02-06 13:23:49 -0200

    nova-remocoes background

--- js/home.js
+++ js/home.js
@@ -417,6 +417,7 @@ function fix_bg_offset() {
   var h = jQuery('#masthead').height();
   jQuery('.search-intro').css('background-position', '0px ' + -h + 'px');
   jQuery('.login-entry').css('background-position', '0px ' + -h + 'px');
+  jQuery('.nova-remocoes').css('background-position', '0px ' + -h + 'px');
   jQuery('.singular').css('background-position', '0px ' + -h + 'px');
 }
 
