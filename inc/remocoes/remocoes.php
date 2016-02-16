8b55ef194c9bb351f1aa8637ab03366fdadde203
Flávio Zavan <flaviozavan@gmail.com>
2016-02-15 23:57:22 -0200

    Use the datepicker for dates

--- inc/remocoes/remocoes.php
+++ inc/remocoes/remocoes.php
@@ -287,8 +287,8 @@ class Remocoes
 	{
 		if(get_query_var(self::NEW_REMOCOES_PAGE) == true)
 		{
-			//wp_enqueue_script('jquery-ui-datepicker-ptbr', WP_CONTENT_URL.'/themes/fluxo/remocoes/js/jquery.ui.datepicker-pt-BR.js', array('jquery-ui-datepicker'));
-			//wp_enqueue_script('date-scripts',WP_CONTENT_URL.'/themes/fluxo/remocoes/js/date_scripts.js', array( 'jquery-ui-datepicker-ptbr'));
+			wp_enqueue_script('jquery-ui-datepicker', get_template_directory_uri().'/inc/remocoes/js/jquery.ui.datepicker.js', array('jquery'));
+			wp_enqueue_script('date-scripts', get_template_directory_uri().'/inc/remocoes/js/date_scripts.js', array('jquery-ui-datepicker'));
 			wp_enqueue_script('new-remocoes', get_template_directory_uri().'/inc/remocoes/js/new-remocoes.js', array( 'jquery'));
 			
 			get_header();
